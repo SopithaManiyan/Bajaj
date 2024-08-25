c64cf990ba677b1b90f61b026f8f60ad1e5f86cd
Sopitha <sopitha.maniyan@gmail.com>
2024-08-25 13:17:23 +0530

    Add route for the root URL

--- backend.py
+++ backend.py
@@ -1,14 +1,11 @@
 from flask import Flask, request, jsonify
 import os
-from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
-app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Hello, World!"
 
 @app.route('/process', methods=['POST'])
 def process_data():
@@ -37,4 +34,5 @@ def get_code():
     return jsonify({"code": 12345})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
