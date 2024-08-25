7bebe4688402304e214d44a16d6ef949acf1f04b
Sopitha <sopitha.maniyan@gmail.com>
2024-08-25 13:05:30 +0530

    Update Flask app to bind to 0.0.0.0 and use PORT environment variable

--- backend.py
+++ backend.py
@@ -1,4 +1,12 @@
 from flask import Flask, request, jsonify
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 
