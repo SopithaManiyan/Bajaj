6fb1c6648d5feca7e9b20f0cd5591a230f4883e5
Sopitha <sopitha.maniyan@gmail.com>
2024-08-25 12:40:23 +0530

    Initial commit

---
+++ backend.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/process', methods=['POST'])
+def process_data():
+    input_data = request.json.get('input_data', [])
+    num_list = [item for item in input_data if item.isdigit()]
+    alpha_list = [item for item in input_data if item.isalpha()]
+    max_lower = max([char for char in alpha_list if char.islower()], default='', key=lambda x: ord(x))
+
+    response = {
+        "status": "success",
+        "user_info": {
+            "user_id": "your_username_date_of_birth",
+            "email": "your_email@example.com",
+            "roll_number": "your_roll_number",
+        },
+        "output": {
+            "numbers": num_list,
+            "alphabets": alpha_list,
+            "max_lowercase": max_lower
+        }
+    }
+    return jsonify(response)
+
+@app.route('/code', methods=['GET'])
+def get_code():
+    return jsonify({"code": 12345})
+
+if __name__ == '__main__':
+    app.run(debug=True)
